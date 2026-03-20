2932a95961f5d85271d304410d59cd428ff802c0
Maria Eduarda Hurla <maria.hurla.oliveira@escola.pr.gov.br>
2026-03-20 20:49:07 -0300

    voltando ao original

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        if (!atual) {
-            console.error('Nenhum passo ativo encontrado!');
-            return;
-        }
-
-        const proximoPassoId = 'passo-' + this.dataset.proximo;
-        const proximoPasso = document.getElementById(proximoPassoId);
-
-        if (!proximoPasso) {
-            console.error(`Não existe elemento com id ${proximoPassoId}`);
-            return;
-        }
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        proximoPasso.classList.add('ativo');
-    });
-});
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
