d0d77505949b4ec4aa1b2745034e0ba0c8f2a920
Maria Eduarda Hurla <maria.hurla.oliveira@escola.pr.gov.br>
2026-03-20 20:35:03 -0300

    vendo se da certo

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ avanca.forEach(button => {
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
 
-        const container = document.getElementById('container-link');
+    })
+})
+
+ const container = document.getElementById('container-link');
 
 // Cria o elemento de âncora (link)
 const link = document.createElement('a');
@@ -17,5 +20,4 @@ link.target = '_blank'; // Abre em nova aba
 
 // Adiciona o link ao contêiner
 container.appendChild(link);
-    })
-})
+
