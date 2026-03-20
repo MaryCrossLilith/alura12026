24ace479fa2ac123f03d975123377e6b2c80e258
Maria Eduarda Hurla <maria.hurla.oliveira@escola.pr.gov.br>
2026-03-20 20:21:04 -0300

    testando o js

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ avanca.forEach(button => {
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
+
+        const container = document.getElementById('container-link');
+
+// Cria o elemento de âncora (link)
+const link = document.createElement('a');
+
+link.target = '_blank'; // Abre em nova aba
+
+// Adiciona o link ao contêiner
+container.appendChild(link);
     })
 })
